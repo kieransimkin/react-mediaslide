4f28f0c56f7deb1b65489e5bc4ad7f60d52c76de
Kieran Simkin <382129+kieransimkin@users.noreply.github.com>
2023-10-20 04:35:58 +0100

    Initial commit

---
+++ src/MediaSlide.js
@@ -0,0 +1,22 @@
+
+import PropTypes from 'prop-types';
+import { useEffect, useRef } from 'react';
+import * as React from "react";
+import { version } from  '../package.json'
+
+const MediaSlide = (props) => { 
+    const {
+
+    } = props;
+
+    return (
+        <>
+            Hello from MediaSlide
+        </>
+    );
+}
+
+MediaSlide.propTypes = {
+   
+  };
+export default MediaSlide;
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import MediaSlide from "./MediaSlide";
+import { version as v } from  '../package.json'
+const version = v;
+const getVersion = () => {
+ return version;
+}
+export {MediaSlide,version,getVersion};
