49a401849bd95ac5f3ffeee9e5a590fbeca3976e
Kieran Simkin <382129+kieransimkin@users.noreply.github.com>
2024-09-06 00:28:19 +0100

    fix scroll on slide mode

--- src/MediaSlide.js
+++ src/MediaSlide.js
@@ -742,6 +742,7 @@ const MediaSlide = (props) => {
 		//scroll();
 		const container = portalDiv.current;
 		const scrollAmount = e.deltaY / 1.5;
+		scroll();
 		container.scrollTo({
 			top: 0,
 			left: container.scrollLeft + scrollAmount,
