7145b25bdd1efd6857f076ff847c08d1fe229c49
Kieran Simkin <382129+kieransimkin@users.noreply.github.com>
2024-09-09 09:45:37 +0100

    1.1.17

--- src/MediaSlide.js
+++ src/MediaSlide.js
@@ -192,7 +192,7 @@ const MediaSlide = (props) => {
 	const [bigInfo, setBigInfo] = useState(
 		initialSelection && typeof doRenderBigInfo === 'function'
 			? doRenderBigInfo(initialSelection, closeBigInfo, goFullscreen, navbarHeight, leftbarOpen)
-			: null
+			: null,
 	);
 	const doLoadingTimer = useCallback(() => {
 		if (loadedPages.length === loadingPages.length) {
@@ -261,7 +261,7 @@ const MediaSlide = (props) => {
 
 			navbarTimer = setTimeout(hideNavbar, 5000);
 
-			if (navbarHeight!=defaultNavbarHidden ? 0 : 60) { 
+			if (navbarHeight != defaultNavbarHidden ? 0 : 60) {
 				setNavbarHeight(defaultNavbarHidden ? 0 : 60);
 				if (window)
 					window.postMessage(
@@ -278,7 +278,7 @@ const MediaSlide = (props) => {
 		clearTimeout(navbarTimer);
 
 		navbarTimer = setTimeout(hideNavbar, 5000);
-		if (navbarHeight!=defaultNavbarHidden ? 0 : 60) { 
+		if (navbarHeight != defaultNavbarHidden ? 0 : 60) {
 			setNavbarHeight(defaultNavbarHidden ? 0 : 60);
 			if (window)
 				window.postMessage(
